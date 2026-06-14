6360a226e188c95b24e713befa8403db2bc45e58
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-06-14 12:16:21 +0530

    Rewrite Android Gradle configuration

--- NexusPlus/shared/build.gradle.kts
+++ NexusPlus/shared/build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
 }
 
 kotlin {
+    jvmToolchain(17)
+
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -39,7 +41,7 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
